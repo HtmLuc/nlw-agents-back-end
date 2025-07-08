f6771cbe69884fb27752e7a86ac83bfd7fc6de34
Lucas Dantas <lucasvdmedeiros@proton.me>
2025-07-08 12:03:14 -0300

    feat: add initial database setup and connection

--- src/server.ts
+++ src/server.ts
@@ -1,25 +1,24 @@
-import { fastifyCors } from '@fastify/cors';
-import { fastify } from 'fastify';
+import { fastifyCors } from '@fastify/cors'
+import { sql } from './db/connection.ts'
+import { fastify } from 'fastify'
 import {
   serializerCompiler,
   validatorCompiler,
   type ZodTypeProvider,
-} from 'fastify-type-provider-zod';
-import { env } from './env.ts';
+} from 'fastify-type-provider-zod'
+import { env } from './env.ts'
 
-const app = fastify().withTypeProvider<ZodTypeProvider>();
+const app = fastify().withTypeProvider<ZodTypeProvider>()
 
 app.register(fastifyCors, {
   origin: 'http://localhost:5173', // Endereço do frontend
-});
+})
 
-app.setSerializerCompiler(serializerCompiler);
-app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler)
+app.setValidatorCompiler(validatorCompiler)
 
 app.get('/health', () => {
-  return 'OK';
-});
+  return 'OK'
+})
 
-app.listen({ port: env.PORT }).then(() => {
-  console.log('HTTP SERVER RUNNING!');
-});
+app.listen({ port: env.PORT })
