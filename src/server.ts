0ada51d9adc18cd3918143f74397c6d78288fb61
Lucas Dantas <lucasvdmedeiros@proton.me>
2025-07-11 17:40:15 -0300

    feat: create question endpoint

--- src/server.ts
+++ src/server.ts
@@ -6,6 +6,9 @@ import {
   type ZodTypeProvider,
 } from 'fastify-type-provider-zod'
 import { env } from './env.ts'
+import { createQuestion } from './http/routes/create-question.ts'
+import { createRoom } from './http/routes/create-room.ts'
+import { getRoomQuestions } from './http/routes/get-room-questions.ts'
 import { getRoomsRoute } from './http/routes/get-rooms.ts'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
@@ -22,5 +25,8 @@ app.get('/health', () => {
 })
 
 app.register(getRoomsRoute)
+app.register(createRoom)
+app.register(getRoomQuestions)
+app.register(createQuestion)
 
 app.listen({ port: env.PORT })
